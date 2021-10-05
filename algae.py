0b2ad5e2e99d0e4f90c8e22fd77ac4b8c8eb8b61
James W. McClain <jmcclain@daystrom-data-concepts.com>
2021-10-05 19:29:54 -0400

    Add Segmentation

--- algae.py
+++ algae.py
@@ -5,19 +5,44 @@ import torch.hub
 import torchvision as tv
 
 
+class AlgaeSegmentationToClassification(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.conv2d = torch.nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1, bias=True).to('cuda')
+
+    def forward(self, x):
+        x = self.pool(x)
+        x = self.conv2d(x)
+        return x
+
+
 class AlgaeClassifier(torch.nn.Module):
     def __init__(self,
                  in_channels: List[int],
                  backbone_str: str,
                  pretrained: bool,
-                 prescale: int):
+                 prescale: int,
+                 chip_size: int = 32):
         super().__init__()
 
         self.backbone_str = backbone_str
         self.prescale = prescale
 
         # Backbone
-        if 'efficientnet_b' in self.backbone_str:
+        if 'fpn' in self.backbone_str and 'resnet' in self.backbone_str:
+            bb = self.backbone_str.split('_')[-1]
+            self.backbone = torch.hub.load(
+                'AdeelH/pytorch-fpn:98c2ea43a9b0118c2e1dc29497bf6c832da5706b',
+                'make_fpn_resnet',
+                name=bb,
+                fpn_type='panoptic',
+                num_classes=2,
+                fpn_channels=256,
+                in_channels=3,
+                out_size=(chip_size, chip_size))
+            self.seg_to_class = AlgaeSegmentationToClassification()
+        elif 'efficientnet_b' in self.backbone_str:
             self.backbone = torch.hub.load(
                 'lukemelas/EfficientNet-PyTorch:7e8b0d312162f335785fb5dcfa1df29a75a1783a',
                 backbone_str,
@@ -29,7 +54,9 @@ class AlgaeClassifier(torch.nn.Module):
             self.backbone = backbone(pretrained=pretrained)
 
         # First
-        if 'efficientnet_b' in self.backbone_str:
+        if 'fpn' in self.backbone_str and 'resnet' in self.backbone_str:
+            self.first = self.backbone[0].m[0][0]
+        elif 'efficientnet_b' in self.backbone_str:
             self.first = self.backbone._conv_stem
         else:
             if self.backbone_str == 'vgg16':
@@ -52,7 +79,9 @@ class AlgaeClassifier(torch.nn.Module):
                 raise Exception(f'Unknown backbone {self.backbone_str}')
 
         # Last
-        if 'efficientnet_b' in self.backbone_str:
+        if 'fpn' in self.backbone_str and 'resnet' in self.backbone_str:
+            self.last = self.seg_to_class
+        elif 'efficientnet_b' in self.backbone_str:
             self.last = self.backbone._fc
         elif self.backbone_str == 'vgg16':
             self.last = self.backbone.classifier[6] = torch.nn.Linear(
@@ -64,8 +93,9 @@ class AlgaeClassifier(torch.nn.Module):
             self.last = self.backbone.classifier = torch.nn.Linear(
                 in_features=2208, out_features=1, bias=True)
         elif self.backbone_str == 'shufflenet_v2_x1_0':
-            self.last = self.backbone.fc = torch.nn.Linear(
-                in_features=1024, out_features=1, bias=True)
+            self.last = self.backbone.fc = torch.nn.Linear(in_features=1024,
+                                                           out_features=1,
+                                                           bias=True)
         elif self.backbone_str == 'mobilenet_v2':
             self.last = self.backbone.classifier[1] = torch.nn.Linear(
                 in_features=1280, out_features=1, bias=True)
@@ -84,10 +114,11 @@ class AlgaeClassifier(torch.nn.Module):
 
         self.cheaplab = torch.nn.ModuleDict()
         for n in in_channels:
-            self.cheaplab[str(n)] = torch.hub.load('jamesmcclain/CheapLab:master',
-                                                   'make_cheaplab_model',
-                                                   num_channels=n,
-                                                   out_channels=3)
+            self.cheaplab[str(n)] = torch.hub.load(
+                'jamesmcclain/CheapLab:master',
+                'make_cheaplab_model',
+                num_channels=n,
+                out_channels=3)
 
     def forward(self, x):
         [w, h] = x.shape[-2:]
@@ -106,13 +137,20 @@ class AlgaeClassifier(torch.nn.Module):
         out = cheaplab(out)
         out = self.backbone(out)
 
+        [w, h] = out.shape[-2:]
+        if len(out.shape) == 4 and w > 1 and h > 1 and w == h:
+            _out = out
+            out = {}
+            out.update({'class': self.seg_to_class(_out)})
+            out.update({'seg': _out})
+        else:
+            out = {'class': out}
+
         return out
 
 
-def make_algae_model(in_channels: List[int],
-                     backbone_str: str,
-                     pretrained: bool,
-                     prescale: int):
+def make_algae_model(in_channels: List[int], backbone_str: str,
+                     pretrained: bool, prescale: int):
     model = AlgaeClassifier(in_channels=in_channels,
                             backbone_str=backbone_str,
                             pretrained=pretrained,
